17c5331488927b48296a1b046a1b03f9e3f242c2
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-09-30 08:42:51 +0200

    searchbar styling

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,7 +1,29 @@
+import Heading from "../Components/Heading";
+import Menu from "../Components/Menu";
 import "./Styling/Search.scss";
 
 export default function Search(){
+    function handleSubmit(){
+
+    }
+    
     return(
-        null
+        <main className="search">
+            <Heading text="Søg" />
+
+            <form onSubmit={handleSubmit} className="search__searchBar">
+                <input
+                    type="text"
+                    id="search"
+                    name="search"
+                />
+                <button
+                    type="submit"
+                    className="fas fa-search">
+                </button>
+            </form>
+
+            <Menu />
+        </main>
     )
 }
