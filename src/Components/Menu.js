a49fa9cd9c23679909f95fde886c67674d052602
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-10-01 11:08:37 +0200

    new icons

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,14 +1,33 @@
 import { Link } from "@reach/router";
 import "./Styling/Menu.scss";
+import {BiHomeAlt} from "react-icons/bi";
+import {FiSearch} from "react-icons/fi";
+import {AiOutlineCalendar,AiOutlineLogin} from "react-icons/ai";
 
 export default function Menu(){
     return(
         <nav className="menu">
             <ul className="menu__list">
-                <Link className="item" to="/aktiviteter"><li className="fas fa-home"></li></Link>
-                <Link className="item" to="/search"><li className="fas fa-search"></li></Link>
-                <Link className="item" to="/kalender"><li className="far fa-calendar"></li></Link>
-                <Link className="item" to="/login"><li className="fas fa-sign-in-alt"></li></Link>
+                <Link className="item" to="/aktiviteter">
+                    <li>
+                        <BiHomeAlt className="icon" />
+                    </li>
+                </Link>
+                <Link className="item" to="/search">
+                    <li>
+                        <FiSearch className="icon" />
+                    </li>
+                </Link>
+                <Link className="item" to="/kalender">
+                    <li>
+                        <AiOutlineCalendar className="icon" />
+                    </li>
+                </Link>
+                <Link className="item" to="/login">
+                    <li>
+                        <AiOutlineLogin className="icon" />
+                    </li>
+                </Link>
             </ul>
         </nav>
     )
